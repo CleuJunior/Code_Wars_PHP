c2b7b2530d44600deaea10a15fdbc524ad0d2f17
CleuJunior <cleu.junior@gmail.com>
2020-03-30 19:49:35 -0300

    Kyu 8 Impares Menores Que N

---
+++ kyu-8/oddCount.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Given a number n, return the number of positive odd numbers below n!
+
+oddCount(7) //=> 3, i.e [1, 3, 5]
+oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+
+*/
+
+function oddCount($n) {
+    $count = 0;
+    for($i = 1; $i < $n; $i++){         
+        if($i % 2 != 0){
+            $count++; 
+        }          
+    }
+    return $count; 
+}
+
+$n = 7;
+
+var_dump(oddCount($n));
+
