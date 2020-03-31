cf96e4494a15975d941fcc3e24dbca9aca21593a
CleuJunior <cleu.junior@gmail.com>
2020-03-30 22:42:31 -0300

    Kyu 8 O Que Beber?

---
+++ kyu-8/drink_about.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples:
+
+people_with_age_drink(13); // => "drink toddy"
+people_with_age_drink(17); // => "drink coke"
+people_with_age_drink(18); // => "drink beer"
+people_with_age_drink(20); // => "drink beer"
+people_with_age_drink(30); // => "drink whisky"
+
+*/
+
+function people_with_age_drink(int $old): string {
+    if($old < 14) return "drink toddy";
+    if($old >= 14 && $old < 18) return "drink coke";
+    if($old >= 18 && $old < 21) return "drink beer";
+    if($old >= 21) return "drink whisky";
+    return "";
+}
+
+/*
+    ----TESTE----
+*/
+
+$old = 13;
+
+echo people_with_age_drink($old);
