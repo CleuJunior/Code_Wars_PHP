58e7402daece5425e84a70201287e982bbf7f04a
CleuJunior <cleu.junior@gmail.com>
2020-06-14 15:32:58 -0300

    Número Único

--- kyu-6/Find_the_unique_number.php
+++ kyu-6/Find_the_unique_number.php
@@ -3,12 +3,8 @@
 // Desafio: https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/php
 
 function find_uniq($a) {
-    $diff = 0;
-    for ($indexI= 0; $indexI < count($a) ; $indexI++) { 
-        if($a[$indexI] != $a[$indexI+1]) return $diff = $a[$indexI+1];
-    }
-  
-    return $diff;
+    sort($a);
+    return $a[0] == $a[1] ? end($a) : $a[0];
 }
 
 
